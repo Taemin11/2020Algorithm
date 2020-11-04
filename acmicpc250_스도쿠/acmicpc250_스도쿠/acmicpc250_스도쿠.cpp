56e3bdcec5d6275d4b7fce9178bf4668e27cdc50
multicampus <haruto4380@gmail.com>
2020-11-04 16:09:53 +0900

    1104 1 success 1 re:solve

---
+++ acmicpc250_스도쿠/acmicpc250_스도쿠/acmicpc250_스도쿠.cpp
@@ -0,0 +1,89 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> map(9, vector<int>(9, 0));
+vector<vector<int>> rows(9, vector<int>(10, 0));
+vector<vector<int>> cols(9, vector<int>(10, 0));
+vector<vector<int>> rooms(9, vector<int>(10, 0));
+bool fire = false;
+
+void dfs(int r, int c) {
+
+	int num_room = (r / 3) * 3 + (c / 3);
+	if (r == 9 && c == 0) {
+		fire = true;
+		return;
+	}
+
+	if (map[r][c] == 0) {
+		for (int i = 1; i <= 9; i++) {
+			if (!rows[r][i] && !cols[c][i] && !rooms[num_room][i]) {
+
+				rows[r][i] = 1;
+				cols[c][i] = 1;
+				rooms[num_room][i] = 1;
+				map[r][c] = i;
+				int dr = r;
+				int dc = c + 1;
+				if (dc == 9) {
+					dr++;
+					dc = 0;
+				}
+				dfs(dr, dc);
+				if (fire) {
+					return;
+				}
+				rows[r][i] = 0;
+				cols[c][i] = 0;
+				rooms[num_room][i] = 0;
+				map[r][c] = 0;
+			}
+		}
+	}
+	else {
+		int dr = r;
+		int dc = c + 1;
+		if (dc == 9) {
+			dr++;
+			dc = 0;
+		}
+		dfs(dr, dc);
+		if (fire) {
+			return;
+		}
+	}
+}
+
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			int input = 0;
+			cin >> input;
+			if (input != 0) {
+				map[i][j] = input;
+				rows[i][input] = 1;
+				cols[j][input] = 1;
+				int r = (i / 3) * 3;
+				int c = (j / 3);
+				rooms[r + c][input] = 1;
+			}
+		}
+	}
+	dfs(0, 0);
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+
+			cout << map[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+
+}
---
+++ acmicpc2550_전구/acmicpc2550_전구/acmicpc2550_전구.cpp
@@ -0,0 +1,88 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N = 0;
+
+	cin >> N;
+
+	vector<int> idx(N + 1); 
+	vector<int> val(N + 1); 
+
+
+	for (int i = 0; i < N; i++) {
+		int input;
+		cin >> input;
+		idx[input] = i; //스위치 번호에 해당하는 순서
+		val[i] = input; //스위치 순서에 해당하는 번호
+	}
+	
+	vector<int> arr(N);
+
+	for (int i = 0; i < N; i++) {
+		int input;
+		cin >> input;
+		arr[i] = idx[input]; //전구에 연결된 스위치 순서
+	}
+	
+	vector<int> lis(N, 987654321);
+	vector<pair<int, int>> trace;
+
+	int lis_size = 0;
+
+	lis[0] = arr[0]; //처음 전구와 연결된 스위치 저장
+	trace.push_back(make_pair(0, arr[0]));
+
+	
+	for (int i = 1; i < N; i++) {
+		//
+		auto it = lower_bound(lis.begin(), lis.end(), arr[i]) - lis.begin();
+		
+		lis[it] = arr[i];
+		trace.push_back(make_pair(it, arr[i]));
+	}
+
+	int len = lis.size();
+	for (int i = 0; i < len; i++) {
+		if (lis[i] != 987654321) {
+			lis_size++;
+		}
+	}
+
+	lis_size--;
+
+	len = trace.size();
+
+	vector<int> ansIdx;
+	for (int i = len - 1; i >= 0; i--) {
+		if (trace[i].first != lis_size)
+			continue;
+
+		ansIdx.push_back(trace[i].second); // idx가 담긴다.    
+		lis_size--;
+	}
+	vector<int> ans;
+
+	// idx를 다시 실제 값으로 변환
+	len = ansIdx.size();
+	for (int i = 0; i < len; i++)
+		ans.push_back(val[ansIdx[i]]);
+
+	// 오름차순
+	sort(ans.begin(), ans.end());
+
+	printf("%d\n", ans.size());
+	for (auto i : ans)
+		printf("%d ", i);
+
+	return 0;	
+
+	
+}
